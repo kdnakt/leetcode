d061497ddaa1a91458c03d45402d962626df13fc
kdnakt <a.kid.1985@gmail.com>
2020-08-31 11:27:30 +0900

    add template project

---
+++ java/template/src/main/java/com/kdnakt/leetcode/Solution.java
@@ -0,0 +1,5 @@
+package com.kdnakt.leetcode;
+
+public class Solution {
+
+}
---
+++ java/template/src/test/java/com/kdnakt/leetcode/SolutionTest.java
@@ -0,0 +1,14 @@
+package com.kdnakt.leetcode;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void testIsTrue() {
+        assertTrue(true);
+    }
+
+}
