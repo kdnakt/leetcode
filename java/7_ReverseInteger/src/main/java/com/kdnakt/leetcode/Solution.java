c77ecce08a1d7d17649b4dbe5a36b4f04ad4773e
kdnakt <a.kid.1985@gmail.com>
2020-09-05 22:02:02 +0900

    7: setup

---
+++ java/7_ReverseInteger/src/main/java/com/kdnakt/leetcode/Solution.java
@@ -0,0 +1,7 @@
+package com.kdnakt.leetcode;
+
+public class Solution {
+    public int reverse(int x) {
+        return x;
+    }
+}
---
+++ java/7_ReverseInteger/src/test/java/com/kdnakt/leetcode/SolutionTest.java
@@ -0,0 +1,25 @@
+package com.kdnakt.leetcode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class SolutionTest {
+
+    private Solution sol = new Solution();
+
+    @Test
+    public void testExample1() {
+        assertEquals(321, sol.reverse(123));
+    }
+
+    @Test
+    public void testExample2() {
+        assertEquals(-321, sol.reverse(-123));
+    }
+
+    @Test
+    public void testExample3() {
+        assertEquals(21, sol.reverse(120));
+    }
+}
