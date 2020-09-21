edc933ec6deca4c15cf41ec2b1d3b4357d4e3c4c
kdnakt <a.kid.1985@gmail.com>
2020-09-22 00:20:33 +0900

    solved 104

---
+++ java/104_MaximumDepthOfBinaryTree/src/test/java/com/kdnakt/leetcode/SolutionTest.java
@@ -0,0 +1,24 @@
+package com.kdnakt.leetcode;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class SolutionTest {
+
+    private Solution sol = new Solution();
+
+    @Test
+    public void testExample1() {
+        TreeNode root = new TreeNode(3,
+                new TreeNode(9),
+                new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+        assertEquals(3, sol.maxDepth(root));
+    }
+
+    @Test
+    public void testNull() {
+        TreeNode root = null;
+        assertEquals(0, sol.maxDepth(root));
+    }
+}
