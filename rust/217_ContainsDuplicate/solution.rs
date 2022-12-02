78361182eb895acf9b4c9f6e3d88ea6b66c068ae
kdnakt <a.kid.1985@gmail.com>
2022-12-02 10:19:15 +0900

    solved 217 with rust

--- rust/217_ContainsDuplicate/solution.rs
+++ rust/217_ContainsDuplicate/solution.rs
@@ -3,7 +3,14 @@ impl Solution {
     pub fn contains_duplicate(
         nums: Vec<i32>
     ) -> bool {
-        false
+        let mut found = std::collections::HashSet::new();
+        for num in nums.iter() {
+            if found.contains(num) {
+                return true;
+            }
+            found.insert(num);
+        }
+        return false;
     }
 }
 
