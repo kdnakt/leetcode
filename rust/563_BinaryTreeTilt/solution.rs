8ed1b91039c6b965e53ff47c41ad7f9ab4e9be9c
kdnakt <a.kid.1985@gmail.com>
2022-12-10 09:15:51 +0900

    solved 563 with rust

--- rust/563_BinaryTreeTilt/solution.rs
+++ rust/563_BinaryTreeTilt/solution.rs
@@ -21,31 +21,25 @@ use std::rc::Rc;
 use std::cell::RefCell;
 impl Solution {
     pub fn find_tilt(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
-        root.as_ref().map_or(0, |r| {
-           let root_node = r.borrow();
-           // TODO: sum left children
-           let left = root_node.left.as_ref().map_or(0, |n| n.borrow().val);
-           let right = root_node.right.as_ref().map_or(0, |n| n.borrow().val);
-           (left - right).abs()
-        }) + root.map_or(0, |r| {
-            let root_node = r.borrow();
-            root_node.left.as_ref().map_or(0, |n| find_tilt(Some(n)))
-                + root_node.right.as_ref().map_or(0, |n| find_tilt(Some(n)))
-        })
+        find_tilt(root.as_ref())
     }
 }
 
 fn find_tilt(root: Option<&Rc<RefCell<TreeNode>>>) -> i32 {
-    root.map_or(0, |r| {
-       let root_node = r.borrow();
-       let left = root_node.left.as_ref().map_or(0, |n| n.borrow().val);
-       let right = root_node.right.as_ref().map_or(0, |n| n.borrow().val);
-       (left - right).abs()
-    }) + root.map_or(0, |r| {
-        let root_node = r.borrow();
-        root_node.left.as_ref().map_or(0, |n| find_tilt(Some(n)))
-            + root_node.right.as_ref().map_or(0, |n| find_tilt(Some(n)))
+    root.as_ref().map_or(0, |r| {
+        let root = r.borrow();
+        let left = root.left.as_ref().map_or(0, |n| sum_subtree(n));
+        let right = root.right.as_ref().map_or(0, |n| sum_subtree(n));
+        (left - right).abs()
+                + find_tilt(root.left.as_ref())
+                + find_tilt(root.right.as_ref())
     })
+}
+
+fn sum_subtree(subtree: &Rc<RefCell<TreeNode>>) -> i32 {
+    let node = subtree.borrow();
+    node.val + node.left.as_ref().map_or(0, |n| sum_subtree(&n))
+        + node.right.as_ref().map_or(0, |n| sum_subtree(&n))
 }
 
 fn tree_node(
@@ -70,4 +64,17 @@ fn main() {
         tree_node(3, None, None)
     );
     assert_eq!(Solution::find_tilt(root), 1);
+
+    // Example 2
+    let root = tree_node(4,
+        tree_node(2,
+            tree_node(3, None, None),
+            tree_node(5, None, None),
+        ),
+        tree_node(9,
+            None,
+            tree_node(7, None, None),
+        )
+    );
+    assert_eq!(Solution::find_tilt(root), 15);
 }
