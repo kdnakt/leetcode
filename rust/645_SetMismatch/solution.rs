01f16a681fae886d59d7cef9ed0995b8fcc83483
kdnakt <a.kid.1985@gmail.com>
2022-12-13 20:55:38 +0900

    solved 645 with rust

--- rust/645_SetMismatch/solution.rs
+++ rust/645_SetMismatch/solution.rs
@@ -1,17 +1,25 @@
 struct Solution;
 impl Solution {
     pub fn find_error_nums(nums: Vec<i32>) -> Vec<i32> {
-        // nums.sort();
-        let mut expected = Vec::new();
-        for i in 1..=nums.len() {
-            expected.push(i as i32);
-        }
-        for (n, e) in nums.iter().zip(expected.iter()) {
-            if n != e {
-                return vec![*n, *e];
+        let mut res = Vec::new();
+        let len = nums.len() as i32;
+        let iter = nums.into_iter();
+        let mut found_zero = false;
+        let mut found_two = false;
+        for i in 1..=len {
+            let c = iter.clone().filter(|x| x == &i).count();
+            if c == 0 {
+                found_zero = true;
+                res.push(i);
+            } else if c == 2 {
+                found_two = true;
+                res.insert(0, i);
+            }
+            if found_zero && found_two {
+                break;
             }
         }
-        Vec::new()
+        return res;
     }
 }
 
