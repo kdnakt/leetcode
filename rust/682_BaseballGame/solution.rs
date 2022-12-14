f0943f0f83e956729fc4e2a573e1d6e399320895
kdnakt <a.kid.1985@gmail.com>
2022-12-15 02:53:30 +0900

    solved 682 with rust

--- rust/682_BaseballGame/solution.rs
+++ rust/682_BaseballGame/solution.rs
@@ -1,9 +1,29 @@
 struct Solution;
 impl Solution {
     pub fn cal_points(operations: Vec<String>) -> i32 {
+        let mut temp = Vec::new();
         operations.into_iter().fold(0, |acc, op| {
+            let len = temp.len();
+            match op.as_ref() {
+                "D" => if let Some(n) = temp.get(len - 1) {
+                    temp.push(n * 2);
+                },
+                "C" => {
+                    temp.pop();
+                },
+                "+" => if let Some(n) = temp.get(len - 1) {
+                    if let Some(m) = temp.get(len - 2) {
+                        temp.push(n + m);
+                    }
+                },
+                _ => match op.parse::<i32>() {
+                    Ok(n) => temp.push(n),
+                    Err(_) => ()
+                }
+            }
             acc
-        })
+        });
+        temp.into_iter().fold(0, |acc,x| acc + x)
     }
 }
 
