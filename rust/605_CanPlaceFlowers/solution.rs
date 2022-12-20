1a2cc78c37c502fdcfb072e65de6e7a097a965f1
kdnakt <a.kid.1985@gmail.com>
2022-12-20 23:38:51 +0900

    605 first submission

--- rust/605_CanPlaceFlowers/solution.rs
+++ rust/605_CanPlaceFlowers/solution.rs
@@ -1,7 +1,55 @@
 struct Solution;
 impl Solution {
     pub fn can_place_flowers(flowerbed: Vec<i32>, n: i32) -> bool {
-        false
+        let mut queue = MyQueue::new();
+        flowerbed.into_iter().for_each(|i| queue.push(i));
+
+        let mut available_count = 0;
+        let mut prev = 0;
+        let mut curr = 0;
+        let mut next = 0;
+        while queue.has_next() {
+            curr = queue.pop();
+            next = queue.peek();
+            if prev == 0 && curr == 0 && next == 0 && queue.has_next() {
+                available_count += 1;
+            }
+            prev = curr;
+        }
+        
+        n == available_count
+    }
+}
+
+struct MyQueue {
+    vec: Vec<i32>,
+    curr: usize,
+}
+impl MyQueue {
+    fn new() -> Self {
+        MyQueue{
+            vec: Vec::new(),
+            curr: 0,
+        }
+    }
+
+    fn push(&mut self, x: i32) {
+        self.vec.push(x);
+    }
+    
+    fn pop(&mut self) -> i32 {
+        let v = *self.vec.get(self.curr).unwrap_or(&0);
+        self.curr += 1;
+        v
+    }
+    
+    fn peek(&mut self) -> i32 {
+        *self.vec.get(self.curr).unwrap_or(&0)
+    }
+    
+    fn has_next(&self) -> bool {
+        let len = self.vec.len();
+        self.curr < len - 1
     }
 }
 
