2edf4cd592dd57301c5b22e661fcde09e5d0b420
kdnakt <a.kid.1985@gmail.com>
2023-01-02 15:16:43 +0900

    setup 733 for rust

---
+++ rust/733_FloodFill/solution.rs
@@ -0,0 +1,41 @@
+struct Solution;
+impl Solution {
+    pub fn flood_fill(
+        image: Vec<Vec<i32>>,
+        sr: i32, sc: i32, color: i32
+    ) -> Vec<Vec<i32>> {
+        image
+    }
+}
+
+fn main() {
+    // Example 1
+    let image = vec![
+            vec![1,1,1],
+            vec![1,1,0],
+            vec![1,0,1],
+    ];
+    let sr = 1;
+    let sc = 1;
+    let color = 2;
+    let expected = vec![
+            vec![2,2,2],
+            vec![2,2,0],
+            vec![2,0,1],
+    ];
+    assert_eq!(Solution::flood_fill(image, sr, sc, color), expected);
+    
+    // Example 2
+    let image = vec![
+            vec![0,0,0],
+            vec![0,0,0],
+    ];
+    let sr = 0;
+    let sc = 0;
+    let color = 0;
+    let expected = vec![
+            vec![0,0,0],
+            vec![0,0,0],
+    ];
+    assert_eq!(Solution::flood_fill(image, sr, sc, color), expected);
+}
