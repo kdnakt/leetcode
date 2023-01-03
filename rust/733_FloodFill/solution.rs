7d9e32ac249f7d09a726ecc92fe215b68b09014b
kdnakt <a.kid.1985@gmail.com>
2023-01-03 18:06:24 +0900

    733 check starting_color first

--- rust/733_FloodFill/solution.rs
+++ rust/733_FloodFill/solution.rs
@@ -4,26 +4,33 @@ impl Solution {
         image: Vec<Vec<i32>>,
         sr: i32, sc: i32, color: i32
     ) -> Vec<Vec<i32>> {
-        image
+        println!("image={:?},sr={sr},sc={sc},color={color}", image);
+        let starting_color = image.get(sr as usize).unwrap().get(sc as usize).unwrap();
+        println!("starting_color={starting_color}");
+        if starting_color == &color {
+            return image;
+        }
+        let mut res = image.clone();
+        res
     }
 }
 
 fn main() {
     // Example 1
-    let image = vec![
-            vec![1,1,1],
-            vec![1,1,0],
-            vec![1,0,1],
-    ];
-    let sr = 1;
-    let sc = 1;
-    let color = 2;
-    let expected = vec![
-            vec![2,2,2],
-            vec![2,2,0],
-            vec![2,0,1],
-    ];
-    assert_eq!(Solution::flood_fill(image, sr, sc, color), expected);
+    // let image = vec![
+    //         vec![1,1,1],
+    //         vec![1,1,0],
+    //         vec![1,0,1],
+    // ];
+    // let sr = 1;
+    // let sc = 1;
+    // let color = 2;
+    // let expected = vec![
+    //         vec![2,2,2],
+    //         vec![2,2,0],
+    //         vec![2,0,1],
+    // ];
+    // assert_eq!(Solution::flood_fill(image, sr, sc, color), expected);
     
     // Example 2
     let image = vec![
