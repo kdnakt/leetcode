3646e5d298ccc9834e946389e36e8194c61936b2
kdnakt <a.kid.1985@gmail.com>
2023-02-01 00:34:00 +0900

    solved 830 with rust

--- rust/830_PositionsOfLargeGroups/solution.rs
+++ rust/830_PositionsOfLargeGroups/solution.rs
@@ -1,9 +1,10 @@
 struct Solution;
 impl Solution {
-    pub fn large_group_positions(s: String) -> Vec<Vec<i32>> {
+    pub fn large_group_positions(mut s: String) -> Vec<Vec<i32>> {
         let mut res = Vec::new();
         let mut curr = ' ';
         let mut start = 0;
+        s.push(' ');
         for (index, c) in s.char_indices() {
             if curr != c {
                 if 2 < index - start {
@@ -31,5 +32,10 @@ fn main() {
     // Example 3
     let s = "abcdddeeeeaabbbcd";
     let expected = vec![vec![3,5],vec![6,9],vec![12,14]];
+    assert_eq!(Solution::large_group_positions(s.to_string()), expected);
+
+    // Failed case
+    let s = "aaa";
+    let expected: Vec<Vec<i32>> = vec![vec![0,2]];
     assert_eq!(Solution::large_group_positions(s.to_string()), expected);
 }
